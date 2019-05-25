40f68d7eba8a21fc36a7e7dfb736b903e80908d2
vagdevi0301 <50985786+vagdevi0301@users.noreply.github.com>
2019-05-25 12:24:48 +0530

    Add files via upload

---
+++ Constructor/src/com/pawana/Constructor/Constructor.java
@@ -0,0 +1,18 @@
+package com.pawana.Constructor;
+
+public class Constructor {
+	{
+		int id=35;
+		String name="devi";
+		 public void Constructor( ) 
+		 {
+		 System.out.println(id+" "+name);
+		 }
+		public static void main(String[] args) 
+			{
+			Constructor a=new Constructor();
+		a.Constructor();
+		}
+		}
+
+}
